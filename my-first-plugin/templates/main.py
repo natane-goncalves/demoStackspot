ba17888d158944bc180b3dbc29687c3ddd72e00c
natanefabene <natanefabene@gmail.com>
2023-07-07 09:52:50 -0300

    Ajustes de Logica

--- my-first-plugin/templates/main.py
+++ my-first-plugin/templates/main.py
@@ -4,6 +4,6 @@ from templateframework.metadata import Metadata
 
 def run(metadata: Metadata = None):
     print("Hello from script.py!")
-    dir = './testeStackspot'       
+    dir = "./{{project_name}}"       
     os.makedirs(dir)
     return metadata
